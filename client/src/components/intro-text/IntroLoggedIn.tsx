65df57cc751dc399c07d88d1cf9901476edbd946
willcary <w.a.c.ary@gmail.com>
2022-12-15 12:42:26 -0500

    Add accordian to UI to display edit user data form

--- client/src/components/intro-text/IntroLoggedIn.tsx
+++ client/src/components/intro-text/IntroLoggedIn.tsx
@@ -1,3 +1,4 @@
+import Accordion from 'react-bootstrap/Accordion'
 import { IntroProps } from '../../assets/ts/types'
 import { UserInfo } from '../../assets/ts/types'
 
@@ -71,44 +72,51 @@ export default function IntroLoggedIn({
         Congratulations on the new member of your family! Add and edit items at
         your leisure.
       </p>
-      <form onSubmit={handleSubmit}>
-        <p>Know the gender of your new little one?</p>
-        <div className='w-50 m-auto mb-3'>
-          <select
-            value={baby_gender}
-            name='baby_gender'
-            onChange={handleUserInfoChange}
-            className='form-select'
-            aria-label='Gender options'
-          >
-            <option value='mystery'>Mystery</option>
-            <option value='girl'>Girl</option>
-            <option value='boy'>Boy</option>
-            <option value='girl-girl'>Two Girls</option>
-            <option value='boy-boy'>Two Boys</option>
-            <option value='boy-girl'>Boy and Girl</option>
-          </select>
-        </div>
+      <Accordion>
+        <Accordion.Item eventKey='0'>
+          <Accordion.Header>Edit profile info</Accordion.Header>
+          <Accordion.Body>
+            <form onSubmit={handleSubmit}>
+              <p>Know the gender of your new little one?</p>
+              <div className='user-input-width m-auto mb-3'>
+                <select
+                  value={baby_gender}
+                  name='baby_gender'
+                  onChange={handleUserInfoChange}
+                  className='form-select'
+                  aria-label='Gender options'
+                >
+                  <option value='mystery'>Mystery</option>
+                  <option value='girl'>Girl</option>
+                  <option value='boy'>Boy</option>
+                  <option value='girl-girl'>Two Girls</option>
+                  <option value='boy-boy'>Two Boys</option>
+                  <option value='boy-girl'>Boy and Girl</option>
+                </select>
+              </div>
 
-        <label htmlFor='address' className='form-label'>
-          Address to send items to:
-        </label>
-        <input
-          type='text'
-          name='address'
-          className='form-control w-50 m-auto mb-3'
-          placeholder='Address'
-          value={address}
-          onChange={handleUserInfoChange}
-          aria-label='Address'
-          aria-describedby='Address to ship'
-        />
-        <input
-          type='submit'
-          value='Submit changes'
-          className='btn btn-primary'
-        />
-      </form>
+              <label htmlFor='address' className='form-label'>
+                Address to send items to:
+              </label>
+              <input
+                type='text'
+                name='address'
+                className='form-control user-input-width m-auto mb-3'
+                placeholder='Address'
+                value={address}
+                onChange={handleUserInfoChange}
+                aria-label='Address'
+                aria-describedby='Address to ship'
+              />
+              <input
+                type='submit'
+                value='Submit changes'
+                className='btn btn-primary'
+              />
+            </form>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </>
   )
 }
